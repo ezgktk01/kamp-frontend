c46a959dcb8c57cff3c66063b10e85419944f12e
ezgi kütük <ezgikutuk3@gmail.com>
2021-03-10 23:19:37 +0300

    HttpClientModule ve http url eklendi,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,9 +15,10 @@ import { NaviComponent } from './components/navi/navi.component';
     CategoryComponent,
     NaviComponent
   ],
-  imports: [
+  imports: [//bizim yazmadıklarımızı buraya yazıyoruz
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/models/product';
+import {HttpClient} from '@angular/common/http';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
 
 @Component({
   selector: 'app-product',
@@ -7,13 +9,20 @@ import { Product } from 'src/app/models/product';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
-
-
+//httpClient ile backend deki dataya ulaşabiliyoruz.
   products:Product[] = [];
+  apiUrl = "https://localhost:44324/api/products/getall";
+  constructor(private httpClient:HttpClient) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {//product açıldığında çalışan kod
+    this.getProducts();//başka bir fonk. çağırırken başına this yazıyoruz
   }
 
+  getProducts(){
+    this.httpClient
+    .get<ProductResponseModel>(this.apiUrl)
+    .subscribe((response)=>{
+      this.products = response.data
+    });//Gelen datayı bu responsemodele map edecez.
+  }
 }

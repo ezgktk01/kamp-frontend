83db2f0570c9c3401ba03fd194074e7222b3a7ad
ezgi kütük <ezgikutuk3@gmail.com>
2021-03-14 23:12:44 +0300

    App Routing'e categoryId olarak ekleme yapıldı.

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,13 +9,19 @@ import { Product } from '../models/product';
 })
 export class ProductService {
 
-  apiUrl = "https://localhost:44324/api/products/getall";
+  apiUrl = "https://localhost:44324/api/";
 
   constructor(private httpClient:HttpClient) { }
 
   getProducts():Observable<ListResponseModel<Product>> {//bir servisten api isteği yapmak için kullanılacak.
+   let newPath = this.apiUrl + "products/getall"
    return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
     //Gelen datayı bu responsemodele map edecez.
   }
 
+  getProductsByCategory(categoryId:number):Observable<ListResponseModel<Product>> {//bir servisten api isteği yapmak için kullanılacak.
+    let newPath = this.apiUrl + "products/getbycategory?categoryId=" + categoryId
+    return this.httpClient.get<ListResponseModel<Product>>(newPath);
+     //Gelen datayı bu responsemodele map edecez.
+   }
 }

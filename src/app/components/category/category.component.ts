014d17dc35d6232f0e1a7c482e181169b4325510
ezgi kütük <ezgikutuk3@gmail.com>
2021-03-14 22:37:00 +0300

    App routing modul ve category componentine ekleme yapıldı.

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -10,6 +10,7 @@ import { CategoryService } from 'src/app/services/category.service';
 export class CategoryComponent implements OnInit {
 
   categories : Category[]=[];
+  currentCategory: Category;
   constructor(private categoryService: CategoryService) { }
 
   ngOnInit(): void {
@@ -21,5 +22,18 @@ export class CategoryComponent implements OnInit {
       this.categories = response.data
     })
     
+  }
+  
+  setCurrentCategory(category: Category){
+    this.currentCategory= category;
+  }
+  
+  getCurrentCategoryClass(category: Category){//hangi kısma tıklandıysa onu mavi renk yapıyor.
+    if(category == this.currentCategory){
+      return "list-group-item active"
+    }
+    else{
+      return "list-group-item "
+    }
   }
 }

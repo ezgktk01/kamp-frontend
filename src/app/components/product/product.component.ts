06c3e2dee385bcae69e5abdf10cc1878d1a35e7e
ezgi kütük <ezgikutuk3@gmail.com>
2021-03-15 00:17:54 +0300

    CategoryId ye göre ürünler listelendi.

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Product } from 'src/app/models/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -12,14 +13,32 @@ export class ProductComponent implements OnInit {
   products:Product[] = [];
   dataLoaded = false;
 
-  constructor(private productService:ProductService) { }
+  constructor(private productService:ProductService,
+     private activatedRoute: ActivatedRoute) { }
 
   ngOnInit(): void {//product açıldığında çalışan kod
-    this.getProducts();//başka bir fonk. çağırırken başına this yazıyoruz
+
+    this.activatedRoute.params.subscribe(params=>{
+      if(params["categoryId"]){
+        this.getProductsByCategory(params["categoryId"])
+      }
+      else{
+        this.getProducts()
+      }
+    })
+    
   }
 
   getProducts(){
     this.productService.getProducts().subscribe(response =>{
+      this.products = response.data
+      this.dataLoaded =true;
+    })
+    
+  }
+
+  getProductsByCategory(categoryId:number){
+    this.productService.getProductsByCategory(categoryId).subscribe(response =>{
       this.products = response.data
       this.dataLoaded =true;
     })
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -9,13 +9,13 @@ import { Product } from '../models/product';
 })
 export class ProductService {
 
-  apiUrl = "https://localhost:44324/api/";
+  apiUrl = 'https://localhost:44324/api/';
 
   constructor(private httpClient:HttpClient) { }
 
   getProducts():Observable<ListResponseModel<Product>> {//bir servisten api isteği yapmak için kullanılacak.
    let newPath = this.apiUrl + "products/getall"
-   return this.httpClient.get<ListResponseModel<Product>>(this.apiUrl);
+   return this.httpClient.get<ListResponseModel<Product>>(newPath);
     //Gelen datayı bu responsemodele map edecez.
   }
 
